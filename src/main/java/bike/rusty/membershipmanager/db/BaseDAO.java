492691b6dce4a3195b3cb8ee67e5baae9ce491d7
Andrew Smith <smith.andrew.l@gmail.com>
2020-12-26 18:09:32 -0600

    Added SQL strings for the SQL DDL statements.

--- src/main/java/bike/rusty/membershipmanager/db/BaseDAO.java
+++ src/main/java/bike/rusty/membershipmanager/db/BaseDAO.java
@@ -8,11 +8,55 @@ public class BaseDAO {
     public static final String CONNECTION_STRING ="jdbc:sqlite:members.db";
 
     Connection connection = DriverManager.getConnection(CONNECTION_STRING);
+    public static final String Club_SQL = String.join(
+        "\n",
+        "CREATE TABLE club(",
+        "    clubId INTEGER PRIMARY KEY AUTOINCREMENT,",
+        "    name VARCHAR(255),",
+        "    description TEXT",
+        ");"
+    );
 
+    public static final String MEMBER_SQL = String.join(
+        "\n",
+        "Create table member(",
+        "    memberId  INTEGER PRIMARY KEY AUTOINCREMENT,",
+        "    firstname VARCHAR(255),",
+        "    lastname  VARCHAR(255),",
+        "    clubId    INTEGER,",
+        "    FOREIGN KEY(clubId) REFERENCES club(clubId)",
+        ");"
+    );
+
+    public static final String CLASS_SQL = String.join(
+            "\n",
+            "Create table class(",
+            "    classId     INTEGER PRIMARY KEY AUTOINCREMENT,",
+            "    name        VARCHAR(255),",
+            "    description TEXT,",
+            "    clubId      INTEGER,",
+            "    FOREIGN KEY(clubId) REFERENCES club(clubid)",
+            ");"
+
+    );
+
+    public static final String CLASS_MEMBER_SQL = String.join(
+            "\n",
+            "create Table class_member(",
+            "    classId INTEGER,",
+            "    memberId INTEGER,",
+            "    primary KEY(",
+            "        classId,",
+            "        memberId",
+            "    )",
+            ");"
+    );
+    
     public BaseDAO() throws SQLException {
         // create the database if it does not exist
         createDatabaseTables();
     }
+
 
     private void createDatabaseTables() {
         createClubTable();
