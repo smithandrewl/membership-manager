2e70d993ba8dc504be57c5d5f2f40924a8702790
Andrew Smith <smith.andrew.l@gmail.com>
2020-12-27 17:05:20 -0600

    Added documentation for Member.java

--- src/main/java/bike/rusty/membershipmanager/model/Member.java
+++ src/main/java/bike/rusty/membershipmanager/model/Member.java
@@ -1,5 +1,8 @@
 package bike.rusty.membershipmanager.model;
 
+/**
+ * Represents a paying club member.
+ */
 public class Member {
     private final int    memberId;
     private final int    clubId;
@@ -22,6 +25,13 @@ public class Member {
         return lastName;
     }
 
+    /**
+     * Creates a new member who has not been saved to the database yet.
+     *
+     * @param clubId    The id of the club they are a member of
+     * @param firstName The members first name
+     * @param lastName  The members last name
+     */
     public Member(
         int    clubId,
         String firstName,
@@ -32,6 +42,15 @@ public class Member {
         this.firstName = firstName;
         this.lastName  = lastName;
     }
+
+    /**
+     * Creates a new member instance when the memberId is already known.
+     *
+     * @param memberId  The members id
+     * @param clubId    The id of the club they are a member at
+     * @param firstName Their first name
+     * @param lastName  Their last name
+     */
     public Member(
         int    memberId,
         int    clubId,
