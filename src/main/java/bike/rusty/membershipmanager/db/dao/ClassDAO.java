b8ae86979afd558a7ffb20090589f2f7bb6852c0
Andrew Smith <smith.andrew.l@gmail.com>
2020-12-27 13:31:08 -0600

    Added a method to get a class by id.

--- src/main/java/bike/rusty/membershipmanager/db/dao/ClassDAO.java
+++ src/main/java/bike/rusty/membershipmanager/db/dao/ClassDAO.java
@@ -2,6 +2,7 @@ package bike.rusty.membershipmanager.db.dao;
 
 import bike.rusty.membershipmanager.model.Class;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,6 +15,17 @@ public class ClassDAO extends BaseDAO {
             "    *,",
             "FROM",
             "    class;"
+        );
+
+    String GET_BY_ID_SQL =
+        String.join(
+            "\n",
+            "SELECT",
+            "    *,",
+            "FROM",
+            "    class",
+            "WHERE",
+            "    classId = ?"
         );
 
     public ClassDAO() throws SQLException {
@@ -32,6 +44,29 @@ public class ClassDAO extends BaseDAO {
             rs.getString("description")
         );
 
+        rs.close();
+        statement.close();
+
+        return clubClass;
+    }
+
+    public Class getClassById(int classId) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(GET_BY_ID_SQL);
+
+        statement.setInt(1, classId);
+
+        ResultSet rs = statement.executeQuery();
+
+        rs.next();
+
+        Class clubClass = new Class(
+            rs.getInt("classId"),
+            rs.getInt("clubId"),
+            rs.getString("name"),
+            rs.getString("description")
+        );
+
+        rs.close();
         statement.close();
 
         return clubClass;
