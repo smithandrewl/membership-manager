35f70886950110b13de8c02e22a2899d50200340
Andrew Smith <smith.andrew.l@gmail.com>
2020-12-27 13:18:47 -0600

    Added a model class for the Class table.

---
+++ src/main/java/bike/rusty/membershipmanager/model/Class.java
@@ -0,0 +1,44 @@
+package bike.rusty.membershipmanager.model;
+
+public class Class {
+    private final int    classId;
+    private final int    clubId;
+    private final String name;
+    private final String description;
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public int getClubId() {
+        return clubId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Class(
+        int    clubId,
+        String name,
+        String description
+    ) {
+        this(-99, clubId, name, description);
+    }
+
+    public Class(
+        int    classId,
+        int    clubId,
+        String name,
+        String description
+    ) {
+        this.classId     = classId;
+        this.clubId      = clubId;
+        this.name        = name;
+        this.description = description;
+    }
+}
