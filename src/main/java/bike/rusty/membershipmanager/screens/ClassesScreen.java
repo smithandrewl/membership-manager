d697909fa6325ee156f0f399cea644f4ddea7772
Andrew Smith <smith.andrew.l@gmail.com>
2020-12-28 20:05:40 -0600

    Added a menu to the classes screen.

--- src/main/java/bike/rusty/membershipmanager/screens/ClassesScreen.java
+++ src/main/java/bike/rusty/membershipmanager/screens/ClassesScreen.java
@@ -5,6 +5,7 @@ import bike.rusty.membershipmanager.db.dao.ClubDAO;
 import bike.rusty.membershipmanager.db.dao.MemberDAO;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ClassesScreen implements IScreen {
     private ClassDAO classDao;
@@ -33,6 +34,26 @@ public class ClassesScreen implements IScreen {
 
     @Override
     public void start() throws SQLException {
+
         System.out.println("Classes screen!");
+
+        while(true) {
+            try {
+
+                System.out.println("Please enter a number:");
+                System.out.println("1. Return to the main menu");
+
+                Scanner scanner = new Scanner(System.in);
+
+                int choice = scanner.nextInt();
+
+                switch(choice) {
+                    case 1:
+                        return;
+                }
+            } catch(Exception e) {
+                System.out.println("Please enter a valid numeric value");
+            }
+        }
     }
 }
