185761c41686fedc4e0a01e57b5f9916673bc518
Andrew Smith <smith.andrew.l@gmail.com>
2020-12-28 21:06:02 -0600

    Added the option to list classes.

--- src/main/java/bike/rusty/membershipmanager/screens/ClassesScreen.java
+++ src/main/java/bike/rusty/membershipmanager/screens/ClassesScreen.java
@@ -3,9 +3,11 @@ package bike.rusty.membershipmanager.screens;
 import bike.rusty.membershipmanager.db.dao.ClassDAO;
 import bike.rusty.membershipmanager.db.dao.ClubDAO;
 import bike.rusty.membershipmanager.db.dao.MemberDAO;
+import bike.rusty.membershipmanager.db.model.Class;
 
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class ClassesScreen implements IScreen {
     private ClassDAO classDao;
@@ -41,14 +43,26 @@ public class ClassesScreen implements IScreen {
             try {
 
                 System.out.println("Please enter a number:");
-                System.out.println("1. Return to the main menu");
 
+                System.out.println("1. List classes");
+                System.out.println("2. Return to the main menu");
                 Scanner scanner = new Scanner(System.in);
 
                 int choice = scanner.nextInt();
 
                 switch(choice) {
                     case 1:
+                        Vector<Class> classes = classDao.getClasses();
+
+                        if(classes.isEmpty()) {
+                            System.out.println("There are currently no classes!");
+                        } else {
+                            for(Class clubClass : classes) {
+                                System.out.println(clubClass);
+                            }
+                        }
+                        break;
+                    case 2:
                         return;
                     default:
                         System.out.println("Error: Please enter a valid option!");
